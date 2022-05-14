57e501220764d23d613667635f2d1224b69ed4f0
Jonapop <70818266+Jonapop@users.noreply.github.com>
2022-05-14 16:13:22 -0700

    Add project files.

---
+++ Game_Relased_On_Birthday/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Game_Relased_On_Birthday
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.richTextBox1.Location = new System.Drawing.Point(226, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(390, 44);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Enter your birthday below";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(162, 143);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(548, 26);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(73, 288);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(674, 150);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Game_Relased_On_Birthday/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Game_Relased_On_Birthday
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string day = dateTimePicker1.Value.Day.ToString();
+            string month = dateTimePicker1.Value.Month.ToString();
+            string year = dateTimePicker1.Value.Year.ToString();
+            Task task = CallToWebAsync(day, month, year);
+        }
+        private async Task<string> CallToWebAsync(string da, string mo, string ye)
+        {
+
+            var httpClient = new HttpClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://www.giantbomb.com/api/game/3030-4725/?api_key=[YOUR_KEY_HERE]&format=json&field_list=genres,name");
+
+            var productValue = new ProductInfoHeaderValue("GitHubProject", "1.0");
+            var commentValue = new ProductInfoHeaderValue("(+https://github.com/Jonapop/Birthday-Game-Release)");
+
+            request.Headers.UserAgent.Add(productValue);
+            request.Headers.UserAgent.Add(commentValue);
+
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(request);
+            var resp = httpResponseMessage;
+            richTextBox2.AppendText(await resp.Content.ReadAsStringAsync());
+            return await resp.Content.ReadAsStringAsync();
+
+        }
+    }
+}
